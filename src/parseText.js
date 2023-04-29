6cac8149f095d4fa23c20a5b550de8d7300d5d3f
funnmer <admin@louiml.net>
2023-04-30 00:59:57 +0300

    Prepare for deployment to GitHub Pages

--- src/parseText.js
+++ src/parseText.js
@@ -71,7 +71,7 @@ const parseText = (inputText) => {
         } else if (matches) {
           return (
             <React.Fragment>
-              <img alt={matches[1]} id="image" height={550} width={600} src={matches[2]} />
+              <img alt={matches[1]} id="image" height={180} width={280} src={matches[2]} />
             </React.Fragment>
           );
         } else if (line.match(regexSettings)) {
